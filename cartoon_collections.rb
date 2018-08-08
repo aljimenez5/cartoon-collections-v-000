e02b715e3822f1ac12ea0f5c7dec8feb371c5340
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 02:06:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(calls)
   calls.any? { |call| call.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_array.include? {|food| 
 end
