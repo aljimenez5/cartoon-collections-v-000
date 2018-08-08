955a290c572e209712b57db061174e751abed231
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-08-08 01:49:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(name_array)
   name_array.each_with_index { |name, index| puts "#{index + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map { |call| puts "#{call.capitalize}!" 
 end
 
 def long_planeteer_calls# code an argument here
